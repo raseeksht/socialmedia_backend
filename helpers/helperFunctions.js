4a7547adc579ad16222315033ea5a1da3214e8fa
raseeksht <raseeksht1@gmail.com>
2024-04-22 15:43:38 +0545

    function to decode auth header and verifyOtp added

--- helpers/helperFunctions.js
+++ helpers/helperFunctions.js
@@ -1,4 +1,6 @@
+import expressAsyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
+import { TOTP } from "totp-generator";
 
 const makeResponse = (status, message, data) => {
     status = status == "s" ? "ok" : "failed";
@@ -14,5 +16,37 @@ const generateJwt = async (payload) => {
     return token;
 }
 
+const verifyOtp = (userOtp, totpSecret) => {
+    const currentTimestamp = Date.now();
+    const validCodes = []
+    console.log(userOtp)
+    // otp based on timestamp for previous, current and next 30s 
+    for (let i = -1; i <= 1; i++) {
+        const timestamp = currentTimestamp + (i * 30000)
+        const { otp } = TOTP.generate(totpSecret, { timestamp });
+        validCodes.push(otp)
+    }
+    if (validCodes.includes(userOtp)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-export { makeResponse, generateJwt };
+const decodeAuthHeaderToken = (req) => {
+    const authToken = req.headers?.authorization
+    if (!(authToken && authToken.startsWith("Bearer "))) {
+        return { error: "Bearer Token Required" };
+    }
+    const token = authToken.split(" ")[1]
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    if (decoded) {
+        console.log(decoded)
+        return decoded
+    } else {
+        return { error: "Token Invalid or expired" }
+    }
+}
+
+
+export { makeResponse, generateJwt, verifyOtp, decodeAuthHeaderToken };
