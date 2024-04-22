e4c8adf8c87bcfa788eb07baf643176adbdf887a
raseeksht <raseeksht1@gmail.com>
2024-04-22 15:47:50 +0545

    route added to handle 2fa funcitons|

---
+++ routes/2fa.routes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { getQR, set2fa } from '../controllers/2fa.controllers.js';
+import fieldValidator from '../middleware/fieldValidator.js';
+// import { verify2fa } from '../controllers/user.controllers.js';
+
+const router = express.Router();
+
+// /api/users/2fa/
+
+router.get("/getqr", getQR);
+
+router.post("/set2fa", fieldValidator(['otp']), set2fa);
+
+// router.post("/verify2fa", fieldValidator(['otp']), verify2fa);
+
+
+export default router;
+
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { createUser, changePassword, getUser, changeName, loginUser } from "../controllers/user.controllers.js";
+import { createUser, changePassword, getUser, changeName, loginUser, afterLoginVerify2fa } from "../controllers/user.controllers.js";
 import fieldValidator from "../middleware/fieldValidator.js";
 import validateUser from "../middleware/validateUser.js";
 import friendRequestRoute from "./friendRequestRoute.js";
+import twoFARoute from './2fa.routes.js';
 
 const router = express.Router();
 
 router.use("/friend-request", validateUser, friendRequestRoute)
+
+router.use("/2fa", validateUser, twoFARoute);
 
 router.post("/", fieldValidator(['firstName', 'lastName', 'email', 'password']), createUser);
 
@@ -14,9 +17,12 @@ router.put("/changepassword/", fieldValidator(['oldPassword', 'newPassword']), v
 
 router.put("/changename/", fieldValidator(['firstName', 'lastName']), validateUser, changeName);
 
+router.post("/login", fieldValidator(['email', 'password']), loginUser);
+
+router.post("/login/verify2fa", fieldValidator(['otp']), afterLoginVerify2fa);
+
 router.get("/:userId", getUser);
 
-router.post("/login", fieldValidator(['email', 'password']), loginUser);
 
 
 
